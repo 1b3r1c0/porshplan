bc3c82e7c6c73d6cb9bec067b59922ef9a1c6e53
zdohmen1 <zdohmen1@gmail.com>
2021-02-09 01:31:09 -0500

    tickers

--- nasdaq_tickers.py
+++ nasdaq_tickers.py
@@ -13,13 +13,14 @@ for line in fh :
 for ticker in tickers :
     try:
         stock = yf.Ticker(ticker)
-        stock_price = stock.info['regularMarketPreviousClose']
-        print(ticker,"-",stock_price)
+        stock_low = stock.info['fiftyTwoWeekLow']
+        stock_current = stock.info['regularMarketPreviousClose']
+        #print(ticker,"-",stock_current,stock_low)
+        if stock_low <= stock_current:
+            percentBelow52WeekLow = 100 * (stock_current - stock_low) / stock_current
+            # debug
+            print(ticker,round(percentBelow52WeekLow, 1))
     except:
         print("wrong ticker")
 
 
-    #try:
-     #   print(stock_price)
-    #except:
-     #   print("nah")
